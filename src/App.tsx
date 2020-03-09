78fd8ef6007a82263c627d3e49b0d2cd7f81b68f
Paul DAO <dao.van.paul@gmail.com>
2020-03-09 16:06:20 +0100

    Event click

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pokemon from "./models/pokemon";
 import POKEMONS from "./models/mock-pokemon";
 function App() {
-  const [pokemons] = useState<Pokemon[]>(POKEMONS);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  
+  useEffect(() => {
+    setPokemons(POKEMONS)
+  }, []);
+
+  const showPokemonsCount = () => {
+    console.log(pokemons.length);
+  };
+
   return (
     <div>
     <h1>Pokedex</h1>
     <p>Il y a {pokemons.length} dans mon Pokedex.</p> 
+    <p onClick={showPokemonsCount}>Afficher le nombre de Pokémons dans la console.</p>
     </div>
   );
 }
