e1a2b0d89edd0b0850a0349b7b0dd4d5911de159
Paul DAO <dao.van.paul@gmail.com>
2020-03-09 16:26:26 +0100

    add link materialize css + create list of pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Pokemon from "./models/pokemon";
 import POKEMONS from "./models/mock-pokemon";
 function App() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  
+
   useEffect(() => {
     setPokemons(POKEMONS)
   }, []);
@@ -12,11 +12,36 @@ function App() {
     console.log(pokemons.length);
   };
 
+
+
   return (
     <div>
-    <h1>Pokedex</h1>
-    <p>Il y a {pokemons.length} dans mon Pokedex.</p> 
-    <p onClick={showPokemonsCount}>Afficher le nombre de Pokémons dans la console.</p>
+        <h1 className="center">Pokedex</h1>
+        <div className="container">
+            <div className="row">
+              {pokemons.map(({id, name, picture, created}) => (
+                <div className="col s6 m4" key={id}>
+
+                    <div className="card horizontal">
+
+                        <div className="card-image">
+                          <img src={picture} alt={name}/>
+                        </div>
+
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <p>{name}</p>
+                                <p><small>{created.toString()}</small></p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+              ))}
+            </div>
+        </div>
     </div>
   );
 }
