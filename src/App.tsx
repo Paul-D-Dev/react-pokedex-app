3228b3d445e3e16dd256cdbd3ab574353210d179
Paul DAO <dao.van.paul@gmail.com>
2020-03-09 15:45:57 +0100

    usestate : display length pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/mock-pokemon";
 function App() {
+  const [pokemons] = useState<Pokemon[]>(POKEMONS);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Pokedex</h1>
+    <p>Il y a {pokemons.length} dans mon Pokedex.</p> 
     </div>
   );
 }
