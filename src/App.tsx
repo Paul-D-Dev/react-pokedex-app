d3de4bab069df8a1abaf958b7307fa26edf513d9
Paul DAO <dao.van.paul@gmail.com>
2020-03-10 10:23:53 +0100

    add page not found in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PokemonList from "./pages/pokemon-list";
 import PokemonsDetail from "./pages/pokemon-detail"
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import PageNotFound from "./pages/page-not-found";
 
 function App() {
 
@@ -17,10 +18,12 @@ function App() {
 
           {/* Le système de gestion de toutes nos routes */}
           {/* Switch permet d'afficher le contenu de la route une seule fois */}
+          {/* React lit les routes de haut en bas, il faut donc mettre la page not found en dernier */}
           <Switch> 
             <Route exact path="/" component={PokemonList}></Route>
             <Route exact path="/pokemons" component={PokemonList}></Route>
             <Route path="/pokemons/:id" component={PokemonsDetail}></Route>
+            <Route component={PageNotFound}></Route>
           </Switch>
       </div>
     </Router>
