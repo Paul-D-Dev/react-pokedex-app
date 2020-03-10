caf4a5b2856c22fdb4c4a08a2f9860dec4b0de2d
Paul DAO <dao.van.paul@gmail.com>
2020-03-10 14:23:10 +0100

    API : convert method in service then to async

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -2,15 +2,15 @@ import Pokemon from "../models/pokemon";
  
 export default class PokemonService {
  
-  static getPokemons(): Promise<Pokemon[]> {
-    return fetch('http://localhost:3001/pokemons')
-      .then(response => response.json());
+  static async getPokemons(): Promise<Pokemon[]> {
+    const response = await fetch('http://localhost:3001/pokemons');
+      return await response.json();
   }
  
-  static getPokemon(id: number): Promise<Pokemon|null> {
-    return fetch(`http://localhost:3001/pokemons/${id}`)
-      .then(response => response.json())
-      .then(data => this.isEmpty(data) ? null : data);
+  static async getPokemon(id: number): Promise<Pokemon|null> {
+    const response = await fetch(`http://localhost:3001/pokemons/${id}`);
+      const data = await response.json();
+      return this.isEmpty(data) ? null : data;
   }
  
   static isEmpty(data: Object): boolean {
