2daf5819d83f0ce049a59bade207740bc70448e0
Paul DAO <dao.van.paul@gmail.com>
2020-03-10 10:09:43 +0100

    set up router navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import PokemonList from "./pages/pokemon-list";
+import PokemonsDetail from "./pages/pokemon-detail"
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
 function App() {
-  
+
   return (
-    <div>
-        <PokemonList />
-    </div>
+    <Router>
+      <div>
+          {/* Barre de navigation à toutes les pages */}
+          <nav>
+            <div className="nav-wrapper teal">
+              <Link to="/" className="brand-logo center">Pokédex</Link>
+            </div>
+          </nav>
+
+          {/* Le système de gestion de toutes nos routes */}
+          {/* Switch permet d'afficher le contenu de la route une seule fois */}
+          <Switch> 
+            <Route exact path="/" component={PokemonList}></Route>
+            <Route exact path="/pokemons" component={PokemonList}></Route>
+            <Route path="/pokemons/:id" component={PokemonsDetail}></Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
