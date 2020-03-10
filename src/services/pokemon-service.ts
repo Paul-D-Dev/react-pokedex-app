c982b64d7ffcea858aa7b3827e5de56cd646db2f
Paul DAO <dao.van.paul@gmail.com>
2020-03-10 14:27:19 +0100

    API : add try catch to handle error

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -2,18 +2,31 @@ import Pokemon from "../models/pokemon";
  
 export default class PokemonService {
  
-  static async getPokemons(): Promise<Pokemon[]> {
-    const response = await fetch('http://localhost:3001/pokemons');
-      return await response.json();
-  }
+    static async getPokemons(): Promise<Pokemon[]> {
+        try {
+            const response = await fetch('http://localhost:3001/pokemons');
+            return await response.json();
+        }
+        catch (error) {
+            return this.handleError(error);
+        }
+      }
  
   static async getPokemon(id: number): Promise<Pokemon|null> {
-    const response = await fetch(`http://localhost:3001/pokemons/${id}`);
-      const data = await response.json();
-      return this.isEmpty(data) ? null : data;
+      try {
+          const response = await fetch(`http://localhost:3001/pokemons/${id}`);
+            const data = await response.json();
+            return this.isEmpty(data) ? null : data;
+      } catch (error) {
+          return this.handleError(error);
+      }
   }
  
   static isEmpty(data: Object): boolean {
     return Object.keys(data).length === 0;
+  }
+
+  static handleError(error: Error): any {
+      console.error(error);
   }
 }
