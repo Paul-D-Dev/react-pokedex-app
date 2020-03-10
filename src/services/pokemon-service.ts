39376c4e033a8e5e0f3f88f4b1a4d7501608a6a5
Paul DAO <dao.van.paul@gmail.com>
2020-03-10 16:52:36 +0100

    API : SEARCH -> method in service (async)

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -67,10 +67,14 @@ export default class PokemonService {
 
     }
 
-    static searchPokemon(term: string): Promise<Pokemon[]> {
-        return fetch(`http://localhost:3001/pokemons?q=${term}`)
-        .then(response => response.json())
-        .catch(error => this.handleError(error))
+    static async searchPokemon(term: string): Promise<Pokemon[]> {
+        try {
+            const response = await fetch(`http://localhost:3001/pokemons?q=${term}`);
+            return await response.json();
+        }
+        catch (error) {
+            return this.handleError(error);
+        }
     }
   
   static isEmpty(data: Object): boolean {
